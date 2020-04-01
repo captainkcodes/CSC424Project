758ec42ef9c8242ad28c19deb0e45526f40ad13b
Marcus Armstrong <49583435+CodingWithMarcus@users.noreply.github.com>
2020-03-31 21:53:40 -0500

    back-end Auth. edits

--- Capstone/public/app.js
+++ Capstone/public/app.js
@@ -1,3 +1,79 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyDGLiieuc_un7gASflZoO5IFxxa_XHM1i0",
+    authDomain: "schedule-me-44d37.firebaseapp.com",
+    databaseURL: "https://schedule-me-44d37.firebaseio.com",
+    projectId: "schedule-me-44d37",
+    storageBucket: "schedule-me-44d37.appspot.com",
+    messagingSenderId: "506796346267",
+    appId: "1:506796346267:web:8b6e69a4db77d3aca62f3e",
+    measurementId: "G-S1HC3BQ9FV"
+  };
+      // Initialize Firebase
+      firebase.initializeApp(firebaseConfig);
+      
+        const auth = firebase.auth();
+        
+        
+        function signUp(){
+            
+            var email = document.getElementById("email");
+            var password = document.getElementById("password");
+            
+            const promise = auth.createUserWithEmailAndPassword(email.value, password.value);
+            promise.catch(e => alert(e.message));
+            
+            alert("Signed Up");
+        }
+        
+        
+        
+        function signIn(){
+            
+            var email = document.getElementById("email");
+            var password = document.getElementById("password");
+            
+            const promise = auth.signInWithEmailAndPassword(email.value, password.value);
+            promise.catch(e => alert(e.message));
+            
+            
+            
+            
+        }
+        
+        
+        function signOut(){
+            
+            auth.signOut();
+            alert("Signed Out");
+            
+        }
+        
+        
+        
+        auth.onAuthStateChanged(function(user){
+            
+            if(user){
+                
+                var email = user.email;
+                alert("Active User " + email);
+                
+                //Take user to a different or home page
+    
+                //is signed in
+                
+            }else{
+                
+                alert("No Active User");
+                //no user is signed in
+            }
+            
+            
+            
+        });
+    
+
+
 document.addEventListener("DOMContentLoaded", event => {
 const app = firebase.app();
 });
