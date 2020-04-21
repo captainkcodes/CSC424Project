2b085130ebef0d2dd6fbdbdc5f49fc333c3de5e6
captainkcodes <idashaglenn@icloud.com>
2020-04-21 17:27:56 -0500

    website overhaul
    added WORKING theme functionality, gave the website a new look

--- Capstone/public/js/theme.js
+++
@@ -1,7 +0,0 @@
-//Theme Toggling
-const checkbox = document.getElementById('checkbox');
-
-checkbox.addEventListener('click', () => {
-  //change the theme of the website
-  document.body.classList.toggle('dark');
-})
