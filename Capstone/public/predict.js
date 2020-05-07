5561f43407cb54e4d0edc62d84199796e9748291
Marcus Armstrong <49583435+CodingWithMarcus@users.noreply.github.com>
2020-05-07 14:11:53 -0500

    exsplained code

--- Capstone/public/predict.js
+++ Capstone/public/predict.js
@@ -36,7 +36,7 @@ function setup() {
   var button = createButton('submit');
   button.mousePressed(predictRatings);
   resultP = createP('');
-
+//create class votings with dropdown menu
   function predictRatings() {
     var newUser = {};
     for (var i = 0; i < dropdowns.length; i++) {
@@ -47,25 +47,25 @@ function setup() {
       }
       newUser[title] = rating;
     }
-
+//algorithm function call
     findNearestNeighbors(newUser);
   }
-
+// algorithm to findNearestNeighbors
   function findNearestNeighbors(user) {
     for (var i = 0; i < resultDivs.length; i++) {
       resultDivs[i].remove();
     }
     resultDivs = [];
-
+//clear results
     var similarityScores = {};
     for (var i = 0; i < data.users.length; i++) {
-      var other = data.users[i];
-      var similarity = euclideanDistance(user, other);
+      var other = data.users[i];//students from dataset
+      var similarity = euclideanDistance(user, other); //compare simularities
       similarityScores[other.name] = similarity;
       
 
     }
-
+ //compare students to findNearestNeighbors
     data.users.sort(compareSimilarity);
 
     function compareSimilarity(a, b) {
@@ -74,13 +74,13 @@ function setup() {
       return score2 - score1;
     }
 
-    for (var i = 0; i < data.titles.length; i++) {
+    for (var i = 0; i < data.titles.length; i++) { //for each class, title = the class data
       var title = data.titles[i];
       if (user[title] == null) {
-        var k = 5;
+        var k = 5;// 5 students
         var weightedSum = 0;
         var similaritySum = 0;
-        for (var j = 0; j < k; j++) {
+        for (var j = 0; j < k; j++) { // calculate sum of votes
           var name = data.users[j].name;
           var sim = similarityScores[name];
           var ratings = data.users[j];
@@ -91,9 +91,9 @@ function setup() {
           }
         }
 
-        var stars = nf(weightedSum / similaritySum, 1, 1);
+        var stars = nf(weightedSum / similaritySum, 1, 1);//calculate total to a 1 decimal point
         var total;
-        if(stars >= 4){
+        if(stars >= 4){//if caculated total is high recommended/ else not recommended
             total = "top recommended"
         }
         else if(stars >= 3){
@@ -105,12 +105,12 @@ function setup() {
         }
 
       
-        var div = createDiv(title + ': ' + total);
+        var div = createDiv(title + ': ' + total);// print totals
 
       
         resultDivs.push(div);
         if(total == "recommended" ||  total == "top recommended"){
-        div.parent(resultP);
+        div.parent(resultP);//if recommended print first
          }
 
 
@@ -121,11 +121,11 @@ function setup() {
   }
 }
 
-function euclideanDistance(ratings1, ratings2) {
-  var titles = data.titles;
+function euclideanDistance(ratings1, ratings2) {//determining simularies via votes in dataset
+  var titles = data.titles; //votes
 
   var sumSquares = 0;
-  for (var i = 0; i < titles.length; i++) {
+  for (var i = 0; i < titles.length; i++) { //for each class 
     var title = titles[i];
     var rating1 = ratings1[title];
     var rating2 = ratings2[title];
@@ -136,7 +136,7 @@ function euclideanDistance(ratings1, ratings2) {
   }
   var d = sqrt(sumSquares);
 
-  var similarity = 1 / (1 + d);
+  var similarity = 1 / (1 + d);//if squared sum is zero we can still divide 1 by the results plus 1.  
   
   return similarity;
 }
